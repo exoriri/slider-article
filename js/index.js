6cb07769128e51ef20a503a393ca67c8d0883857
exoriri <lavrikrom2@gmail.com>
2022-08-24 13:22:28 -0700

    feat: final version

--- js/index.js
+++ js/index.js
@@ -9,14 +9,20 @@ for (let i = slider.children.length - 1; i >= 0; i--) {
 const arrowBtnLeft = document.querySelectorAll('.arrow-btn')[0];
 const arrowBtnRight = document.querySelectorAll('.arrow-btn')[1];
 const sliderItems = document.querySelectorAll('.slider__item');
+const firstSliderItem = sliderItems[0];
+const lastSliderItem = sliderItems[sliderItems.length - 1];
 const dots = document.querySelectorAll('.dot');
 
 let countItems = sliderItems.length;
 let activeIndex = 0;
-let position = 0;
 let sliderElemWidth = document.querySelectorAll('.slider__item')[0].offsetWidth;
+let position = sliderElemWidth;
 
-arrowBtnRight.addEventListener('click', function() {
+slider.style.transform = `translateX(-${position}px)`;
+slider.insertAdjacentElement('afterbegin', lastSliderItem.cloneNode(true));
+slider.insertAdjacentElement('beforeend', firstSliderItem.cloneNode(true));
+
+const onRightBtnClick = () => {
     activeIndex += 1;
 
     if (activeIndex === countItems) {
@@ -24,15 +30,14 @@ arrowBtnRight.addEventListener('click', function() {
         console.log(activeIndex)
         activeIndex = 0;
         slider.style.transform = `translateX(-${position + sliderElemWidth}px)`;
-        slider.insertAdjacentElement('beforeend', sliderItems[activeIndex].cloneNode(true));
         dots[0].classList.add('dot--active');
         dots[countItems - 1].classList.remove('dot--active');
         
         setTimeout(() => {
-            slider.innerHTML = sliderItemsText;
-            position = 0;
+            // slider.innerHTML = sliderItemsText;
+            position = sliderElemWidth;
             slider.style.transition = 'none';
-            slider.style.transform = `translateX(0px)`;
+            slider.style.transform = `translateX(-${position}px)`;
         },300);
 
     } else {
@@ -42,29 +47,44 @@ arrowBtnRight.addEventListener('click', function() {
         dots[activeIndex].classList.add('dot--active')
         dots[activeIndex-1].classList.remove('dot--active');
     }
-});
+};
 
-arrowBtnLeft.addEventListener('click', function() {
+const onLeftBtnClick = () => {
     activeIndex -= 1;
     if (activeIndex === -1) {
         slider.style.transition = 'transform .3s';
-        slider.style.transform = `translateX(${position+sliderElemWidth}px)`;
-        slider.insertAdjacentElement('afterbegin', sliderItems[countItems-1].cloneNode(true));
+        //due to our default position value is sliderElemWidth, but our last element is actually hidden first element we put 0px to show it
+        slider.style.transform = `translateX(0px)`;
         setTimeout(() => {
-            slider.innerHTML = sliderItemsReversedText;
             slider.style.transition = 'none';
-            slider.style.transform = `translateX(0px)`;
+            position = sliderElemWidth * countItems - 2;
+            slider.style.transform = `translateX(-${position}px)`;
         },300);
         activeIndex = countItems - 1;
         dots[activeIndex].classList.add('dot--active');
         dots[0].classList.remove('dot--active');
     } else {
-        slider.insertAdjacentElement('afterbegin', sliderItems[countItems-1].cloneNode(true));
-        position += sliderElemWidth;
-        slider.style.transform = `translateX(+${position}px)`;
-        // slider.style.transform = `translateX(${position}px)`;
-        sliderItems[countItems-1].remove();
-        // dots[activeIndex].classList.add('dot--active')
-        // dots[activeIndex+1].classList.remove('dot--active')
+        slider.style.transition = 'transform .3s';
+        position -= sliderElemWidth;
+        slider.style.transform = `translateX(-${position}px)`;
+        dots[activeIndex].classList.add('dot--active')
+        dots[activeIndex+1].classList.remove('dot--active')
     }
-});
+};
+
+arrowBtnRight.addEventListener('click', debounce(onRightBtnClick, 300));
+arrowBtnLeft.addEventListener('click', debounce(onLeftBtnClick, 300));
+
+function debounce(func, ms) {
+    let isFuncProcessing = false
+    return function() {
+        if (isFuncProcessing) return;
+
+        func.apply(this, arguments);
+
+        isFuncProcessing = true;
+        setTimeout(() => {
+            isFuncProcessing = false;
+        }, ms)
+    }
+}
